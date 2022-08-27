87543a7bbfe20cfe91f44e4a8aff6e99a6993b37
Travis Mendez <Travis.mendez779@gmail.com>
2022-08-26 18:25:14 -0700

    commit tree 285f73a94a71c134f11b754eae3f6f5164a09725

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+
+return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun("bathe my dog");
+
+function mondayWork (work = "go to the office") {
+
+    return `This Monday, I will ${work}.`;
+}
+mondayWork("work from home.");
+
+function wrapAdjective (flair = "*"){
+    const innerFunction = function (adj = 'special'){
+        return `You are ${flair}${adj}${flair}!`
+    }
+    return innerFunction
+}
+
+wrapAdjective('|')('radical');
